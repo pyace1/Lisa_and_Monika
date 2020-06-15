b4151dc36e52f754fe4d376d28951852c37fb356
Your Name <you@example.com>
2020-06-15 18:42:19 +0200

    fotke

---
+++ scripts/navbar.js
@@ -0,0 +1,18 @@
+/* only execute this script when the document is ready */
+$(document).ready(function(){
+  /* function called when you click of the button */
+  $("button").click(function(){
+      
+      /* this if else to change the text in the button */
+      if($("button").text() == "☰"){
+       $("button").text("🞬");
+      }else{
+        $("button").text("☰");
+      }
+    
+    /* this function toggle the visibility of our "li" elements */
+    $("li").toggle("slow");
+  });
+});
+
+/* link gde je pokupljen hamburger menu https://itnext.io/how-to-create-a-responsive-navigation-bar-with-html-css-and-jquery-step-by-step-tutorial-9c780b58479f */
